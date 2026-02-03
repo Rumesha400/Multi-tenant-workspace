2e506291ce1ad2648eadf5aa4955182ddfead1e0
Rumesha400 <rumesha0204@gmail.com>
2026-02-03 18:09:18 +0530

    Initial frontend setup with React, TypeScript, Tailwind, and shadcn/ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return <div>App Loaded
+
+    <div className="text-red-500 text-xl">Tailwind Works</div>
+  <Button>Works</Button>
+  </div>
+}
+
+export default App
